bc200fc5c2e087b5fad49a06b8dc5d35ada0e0db
Eliezer Portillo <eliezerportillo7@gmail.com>
2022-06-04 17:51:05 -0500

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'taquilla-virtual',
+    appId: '1:440288510222:web:96e5fbde9b87880d58175f',
+    storageBucket: 'taquilla-virtual.appspot.com',
+    locationId: 'us-west2',
+    apiKey: 'AIzaSyD1h5ynQbUt2daBkSKLOttdhe9HeiusgUk',
+    authDomain: 'taquilla-virtual.firebaseapp.com',
+    messagingSenderId: '440288510222',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'taquilla-virtual',
+    appId: '1:440288510222:web:96e5fbde9b87880d58175f',
+    storageBucket: 'taquilla-virtual.appspot.com',
+    locationId: 'us-west2',
+    apiKey: 'AIzaSyD1h5ynQbUt2daBkSKLOttdhe9HeiusgUk',
+    authDomain: 'taquilla-virtual.firebaseapp.com',
+    messagingSenderId: '440288510222',
+  },
   production: false
 };
 
